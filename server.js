2b148615b154d0f1e3cb83f474fc5a302cc0b7fa
phg02 <s4063236@rmit.edu.vn>
2024-04-21 15:10:55 +0700

    put

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname+"./views");
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
+const indexRouter = require('./routes/index');// import route or index
 
+//setting up mongoose
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err)); //log error of mongodb
+db.once( 'open', ()=> console.log('Connected to mongoose')); // log when mongo is connected
 
+app.set('view engine', 'ejs'); 
+app.set('views', __dirname+"/views"); //setting up views folder
+app.set('layout', 'layouts/layout'); // settin up layout (optional)
+app.use(expressLayouts); // optional(help for repetitve header and and footer)
+app.use(express.static('public')); 
+
+app.use('/', indexRouter);// use router of index.js from folder routes
 
 app.listen(process.env.PORT || 3000);
